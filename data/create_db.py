b6f3a99b61b0261f78bfc3c2a271b256e7c0777a
Lena Merkli <lena@merkli.me>
2025-05-02 18:48:41 +0200

    Implement vector search database

---
+++ data/create_db.py
@@ -0,0 +1,27 @@
+import chromadb
+from os.path import abspath, exists
+from os import listdir
+import typing as t
+from json import load as json_load
+
+
+def main() -> None:
+    if exists(abspath('./database.chroma')):
+        return None
+    client = chromadb.PersistentClient(path=abspath('./database.chroma'))
+    collection = client.create_collection(name='PolitScanner')
+    for file in listdir(abspath('./parsed/')):
+        if file.endswith('.json') and file != 'hashes.json':
+            with open(abspath(f'./parsed/{file}'), 'r') as f:
+                data: t.Dict[str, t.Any] = json_load(f)
+            for i, statement in enumerate(data['original_statements']):
+                collection.add(
+                    documents=[statement],
+                    ids=[f"{file.rsplit('.', 1)[0]}-{i}"]
+                )
+    return None
+
+
+
+if __name__ == '__main__':
+    main()
